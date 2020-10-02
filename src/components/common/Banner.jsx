b8cb80e3c8bf9f0f7679a4511d08c84310f5a310
waelabd <wael@dagoma.fr>
2020-10-02 17:50:09 +0200

    Pages Extruder+ - rename addon path - new pages extruder+ - add option background to Banner

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -1,5 +1,6 @@
 import React, { Fragment } from 'react';
 import { useTranslation } from 'react-i18next';
+import Item from './Item';
 
 function Title({ content }) {
   const { t } = useTranslation();
@@ -7,23 +8,35 @@ function Title({ content }) {
   return <p className="big-title">{t(content)}</p>;
 }
 
-function SubTitle({ small, content }) {
+function SubTitle({ content, small }) {
   const { t } = useTranslation();
   if (!content) return null;
   const classes = small ? (content.length === 1 ? ['col-vspace'] : ['col-vtspace', 'col-vbspace']) : null;
+  const getItem = (i, k) => {
+    if (typeof i == 'string') return <p key={k} className={classes ? classes[k] : null}>{t(i)}</p>
+    return <Item content={i} />
+  }
   return (
     <Fragment>
-      {content.map((i, k) => <p key={k} className={classes ? classes[k] : null}>{t(i)}</p>)}
+      {content.map((i, k) => getItem(i, k))}
     </Fragment>
   );
 }
 
-function Banner({ small, content }) {
+function Banner({ content, small }) {
   if (!content) return null;
 
-  const { title, subtitle } = content;
-  const sectionClasses = (small) ? 'col-xl-24 bg-gradient-orange row' : 'col-xl-24 block-big-white-space bg-gradient-orange row';
+  const { title, subtitle, background } = content;
+  const getBackground = (style) => {
+    const background = {
+      "default": "bg-gradient-orange",
+      "grey": "bg-light-grey-blue",
+      "transparent": " "
+    }
+    return background[style] || background["default"]
+  }
 
+  const sectionClasses = ((small) ? 'col-xl-24 row' : 'col-xl-24 block-big-white-space row') + ` ${getBackground(background)}`;
   return (
     <section className={sectionClasses}>
       <Title content={title} />
@@ -33,7 +46,7 @@ function Banner({ small, content }) {
 }
 
 Banner.defaultProps = {
-  small: false
+  small: false,
 }
 
 export default Banner;
