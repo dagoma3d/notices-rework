f6ee15eda4eba735ee7eb0310e06da3cce3584d2
benjamindago <benjamin@dagoma.fr>
2022-06-21 14:22:18 +0200

    Add Notice PRO 430
    Notice PRO 430 other part

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Header from './components/common/Header';
 import Home from './pages/Home';
+import HomePro from './pages/HomePro';
 import Page from './pages/Page';
 import NotFound from './components/common/NotFound';
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -14,6 +15,7 @@ export default function App() {
       <Header />
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route exact path="/pro" component={HomePro} />
         <Route path="/:p0/:p1?/:p3?" component={Page} />
         <Route component={NotFound} />
       </Switch>
---
+++ src/pages/HomePro.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment, useEffect, useState } from 'react';
+import Choices from '../components/home/Choices';
+import Ribbon from '../components/common/Ribbon';
+import Banner from '../components/common/Banner';
+
+function Home() {
+  const [content, setContent] = useState();
+
+  useEffect(() => {
+    fetch('./content/homePro.json').then((response) => {
+      response.json().then((data) => {
+        setContent(data);
+      });
+    });
+  }, []);
+
+  if (!content) return null;
+
+  return (
+    <Fragment>
+      <Ribbon content={content.header} />
+      <Banner content={content.title} />
+      <Choices content={content.choices} />
+    </Fragment>
+  );
+}
+
+export default Home;
